0460548eaffff09745a62b677101397b564a59a4
John_B <john@birchall.info>
2024-06-19 08:34:54 +0000

    add autocomplete=off to filter sidebar to stop Chrome changing settings when back button used

--- wp-content/themes/oph/part/filter-sidebar.php
+++ wp-content/themes/oph/part/filter-sidebar.php
@@ -84,7 +84,7 @@ if (isset($params['q'][0])) {
 }
 
 $return_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https' : 'http') . '://' .  $_SERVER['HTTP_HOST'];
-$return_url = $return_url . $_SERVER["REQUEST_URI"];
+$return_url = $return_url . urldecode($_SERVER["REQUEST_URI"]);
 $return_url = explode("?", $return_url)[0];
 $return_url = explode("page", $return_url)[0];
 
@@ -97,7 +97,7 @@ $return_url = explode("page", $return_url)[0];
 				<span class="sidebar-filter-hide-button-text">Hide Options</span> <span class="sidebar-filter-hide-icon"></span>
 			</button>
 		</div> -->
-		<form method="GET" class="sidebar-filter__content" action="<?php echo $return_url; ?>">
+		<form method="GET" class="sidebar-filter__content" action="<?php echo $return_url; ?>" autocomplete="off">
 			<div class="sidebar-filter__search">
 				<!-- search bar -->
 				<input class="selection-query" type="search" name="q" placeholder="Search" value="<?php if( isset($search_query) ) { echo $search_query; } ?>">
